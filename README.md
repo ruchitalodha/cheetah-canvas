e4fb303278cba5a3f237a15f6aaa0f6e0cc0bc4f
Ruchita <lodha.ru@gmail.com>
2019-06-08 14:40:37 +0530

    Added basic canvas with changeable colors and colorpicker

---
+++ README.md
@@ -0,0 +1,7 @@
+# cheetah-canvas
+This is a drawing canvas written in vanilla javascript.
+Actions:
+1. Draw by moving the mouse to the canvas. Default color is Red.
+2. Change color using the keys R G B Y.
+3. Use color picker to change color.
+4. For small devices colors cannot be changed.
---
+++ js/index.js
@@ -0,0 +1,75 @@
+
+
+var currentColor = 'red'
+var letsdraw = false;
+var upArrowClicked = false;
+var canvas = document.getElementById("canvas");
+var colorPicker = document.getElementById("colorpicker");
+var w = canvas.width = window.innerWidth * 0.8;
+var h = canvas.height = window.innerHeight* 0.8;
+var ctx = canvas.getContext('2d');
+ctx.fillStyle = 'red';
+var currentX = '';
+var currentY = '';
+var keycodes = {
+  66: "blue",
+  71: "green",
+  89: "yellow",
+  82: "red",
+}
+
+function getMousePos(event) {
+	return {
+		x: event.clientX - canvas.offsetLeft - 20,
+		y: event.clientY - canvas.offsetTop -20,
+	};
+}
+
+function onMouseMove() {
+  canvas.addEventListener("mouseenter", function(e) {
+    letsdraw = true;
+  });
+
+  canvas.addEventListener("mousemove", function(e) {
+    var mousePos = getMousePos(e);
+    currentX = mousePos.x;
+    currentY = mousePos.y;
+    if(letsdraw && !upArrowClicked){
+      ctx.fillRect(mousePos.x, mousePos.y, 40, 40)
+    }
+  });
+}
+
+//On load set the keybindings of document and the mousemove action.
+// window.addEventListener('load', function() {
+  document.onmousemove = onMouseMove();
+  document.addEventListener('keyup', function(e) {
+    //If the key is spacebar clear canvas;
+    e.preventDefault();
+    if(e.keyCode == 32) {
+      ctx.clearRect(0, 0, w, h);
+      colorPicker.click();
+    }
+
+    //Up arrow to stop drawing
+    if(e.keyCode == 38) {
+      upArrowClicked = true;
+    }
+
+    //Down arrow to start drawing
+    if(e.keyCode == 40) {
+      upArrowClicked = false;
+    }
+
+    //If key is b/g/y/r then change the stroke to respective keycode
+    //and then make a stroke on the current position
+    else if ([66,71,89,82].includes(e.keyCode)){
+      ctx.fillStyle = keycodes[e.keyCode];
+      ctx.fillRect(currentX, currentY, 40, 40)
+    }
+  });
+
+  colorPicker.addEventListener("change", function(e) {
+    ctx.fillStyle = e.target.value;
+  });
+// })
