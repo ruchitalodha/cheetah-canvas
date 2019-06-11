176530dc544d505c81373812d75ad6f03be32bde
Ruchita <lodha.ru@gmail.com>
2019-06-11 16:54:43 +0530

    Minor changes

--- js/index.js
+++ js/index.js
@@ -2,8 +2,10 @@ var letsdraw = false;
 var upArrowClicked = false;
 var canvas = document.getElementById("canvas");
 var colorPicker = document.getElementById("colorpicker");
-var w = canvas.width = window.innerWidth * 0.8;
-var h = canvas.height = window.innerHeight * 0.8;
+var aspectRatio = 0.8;
+var brushSize = 40;
+var w = canvas.width = getDeviceDimensions().w * aspectRatio;
+var h = canvas.height = getDeviceDimensions().h * aspectRatio;
 var ctx = canvas.getContext('2d');
 ctx.fillStyle = 'red';
 var currentX = '';
@@ -14,11 +16,11 @@ var keycodes = {
   89: "yellow",
   82: "red",
 };
-
+var isMobileDevice = getDeviceDimensions().w < 700
 function getMousePos(event) {
   return {
-    x: event.clientX - canvas.offsetLeft - 20,
-    y: event.clientY - canvas.offsetTop - 20,
+    x: event.clientX - canvas.offsetLeft - brushSize/2,
+    y: event.clientY - canvas.offsetTop - brushSize/2,
   };
 }
 
@@ -32,13 +34,16 @@ function onMouseMove() {
     currentX = mousePos.x;
     currentY = mousePos.y;
     if (letsdraw && !upArrowClicked) {
-      ctx.fillRect(mousePos.x, mousePos.y, 40, 40);
+      ctx.fillRect(mousePos.x, mousePos.y, brushSize, brushSize);
     }
   });
 }
 
-function isMobileDevice() {
-  return (typeof window.orientation !== "undefined") || (navigator.userAgent.indexOf('IEMobile') !== -1);
+function getDeviceDimensions() {
+  return {
+    w: window.innerWidth,
+    h: window.innerHeight
+  }
 }
 
 function callKeyBindingEvents() {
@@ -75,11 +80,45 @@ colorPicker.addEventListener("change", function (e) {
   ctx.fillStyle = e.target.value;
 });
 
-if (!isMobileDevice()) {
-  callKeyBindingEvents();
+//On resize get the device type
+window.addEventListener("resize", onResizeFunction);
+function onResizeFunction (e){
+  isMobileDevice = getDeviceDimensions() < 700
+}
+
+if (!isMobileDevice) {
+  document.addEventListener('keyup', function (e) {
+    //If the key is spacebar clear canvas;
+    e.preventDefault();
+    if (e.keyCode == 32) {
+      ctx.clearRect(0, 0, w, h);
+      colorPicker.click();
+    }
+
+    //Up arrow to stop drawing
+    if (e.keyCode == 38) {
+      upArrowClicked = true;
+    }
+
+    //Down arrow to start drawing
+    if (e.keyCode == 40) {
+      upArrowClicked = false;
+    }
+
+    //If key is b/g/y/r then change the stroke to respective keycode
+    //and then make a stroke on the current position
+    else if ([66, 71, 89, 82].includes(e.keyCode)) {
+      ctx.fillStyle = keycodes[e.keyCode];
+      ctx.fillRect(currentX, currentY, brushSize, brushSize);
+    }
+  });
 }
 else {
+  console.log(getDeviceDimensions(), w, h);
   window.addEventListener("orientationchange", function() {
-  ctx.clearRect(0, 0, w, h);
-  }, false);
+  w = canvas.width = getDeviceDimensions().w * aspectRatio;
+  h = canvas.height = getDeviceDimensions().h * aspectRatio;
+  // ctx.clearRect(0, 0, w, h);
+  console.log('or change', getDeviceDimensions(), w, h);
+}, true);
 }
